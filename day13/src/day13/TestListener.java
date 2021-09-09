89585a43cb0f0c39254a9ec68ef8af814542f064
OHT <gus3578@naver.com>
2021-09-09 15:56:06 +0900

    웹 리스너 간단한 실습, 서버 실행시 데이터 공급

---
+++ day13/src/day13/TestListener.java
@@ -0,0 +1,37 @@
+package day13;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * Application Lifecycle Listener implementation class TestListener
+ *
+ */
+@WebListener
+public class TestListener implements ServletContextListener {
+
+    /**
+     * Default constructor. 
+     */
+    public TestListener() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+     * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+     */
+    public void contextDestroyed(ServletContextEvent arg0)  { 
+         // TODO Auto-generated method stub
+    }
+
+	/**
+     * @see ServletContextListener#contextInitialized(ServletContextEvent)
+     */
+    public void contextInitialized(ServletContextEvent sce)  { 
+         ServletContext context = sce.getServletContext();
+         context.setAttribute("student", new Student("ȫ�浿",97));
+    }
+	
+}
